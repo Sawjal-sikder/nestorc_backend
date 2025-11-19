609ca9f46a99cc9e5abf2f982287559e66b09f55
sajal <mdsajalsikder@gmail.com>
2025-11-19 12:15:08 +0600

    Add account deletion view and update URLs
    - Added a new view function `accountdeleteView` in `views.py` to handle account deletion requests.
    - Updated `urls.py` to include a new URL pattern for the account deletion view.
    - Ensured that the privacy policy view remains unchanged while integrating the new functionality.

--- privacy/urls.py
+++ privacy/urls.py
@@ -1,5 +1,7 @@
-from .views import privacy_policy
+from .views import accountdeleteView, privacy_policy
 from django.urls import path
+
 urlpatterns = [
     path('privacy-policy/', privacy_policy, name='privacy_policy'),
+    path('account-delete/', accountdeleteView, name='account_delete'),
 ]
--- privacy/views.py
+++ privacy/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def privacy_policy(request):
     # return render(request, 'privacy.html')
     return render(request, 'new.html')
+
+
+def accountdeleteView(request):
+    return render(request, 'accountDelete.html')
